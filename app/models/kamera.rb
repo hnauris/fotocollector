eeb7b807b7321b654d7c4caf034dc39073429384
hnauris <nauris.horsts@gmail.com>
2018-01-13 02:14:33 +0200

    Truncate removed from integers

--- app/models/kamera.rb
+++ app/models/kamera.rb
@@ -1,4 +1,5 @@
 class Kamera < ApplicationRecord
+    include ActionView::Helpers::TextHelper
     belongs_to :user
     has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ""
     validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
